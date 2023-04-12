6bb3e1fcc4a8d1db3896f957c69c0d67d2b332c4
Ankita Dodamani <46243069+D-Ankita@users.noreply.github.com>
2023-04-12 13:43:17 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+  production: true
+};
+
+export const Config = {
+  api_key: 'blte5c0a8f1b14d92b5',
+  delivery_token: 'csb565b6e7a01d747c60275eed',
+  environment: 'development',
+  region: 'us',
+  management_token:'cs7308478e4ec42d60bc876b82',
+  api_host:'api.contentstack.io',
+  app_host:'app.contentstack.com',
+  live_preview:true
+};
